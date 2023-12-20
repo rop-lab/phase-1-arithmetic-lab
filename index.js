cf9beea0929a6bb94ab2d549dc9f2c5afb6aa3ed
rop-lab <cheruiyot.rop@student.moringaschool.com>
2023-12-20 22:54:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// write your code her
+// set variable num1 and num2
+let num1=31;
+let num2=2;
 
-// Write your code here
+// multiply num1 and num2
+letmultiply= num1 * num2;
+
+//console.log results
+console.log(multiply);
+
+let random= Math.floor(Math.random () * 100)+1;
+console.log(random);
+
+//declare variables num3 and num4
+let num3 = 16;
+let num4 =12;
